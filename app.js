aa047d8b92d76bf91c000755d82faea9dbd43782
endadinh <endadinh@gmail.com>
2021-10-29 12:27:28 +0700

    add CORS

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ const app = express();
 const notiRoute = require('./routes/noti');
 
 app.use(bodyParser.json());
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use("/noti",notiRoute);
 
 
